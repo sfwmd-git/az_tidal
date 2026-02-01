910ea2f350e3399188c7d31df4e2c40213b3da20
Nate Taminger <nate@Nates-MacBook-Pro.local>
2026-01-31 18:34:28 -0600

    Remove all files to prepare for new structure

--- download_all_euro.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run all the download scripts
-./download_euro_air_deterministic.sh
-./download_euro_air_ensemble.sh
-./download_euro_wave_deterministic.sh
-./download_euro_wave_ensemble.sh
--- get_data_together.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python=/usr/bin/python3
-path=/home/ntaminge/SFWMD_work/predictions
-today=$(date +%Y-%m-%d)
-
-$python $path/py_w_Get\ Data\ Together.py
-
-exit 0
